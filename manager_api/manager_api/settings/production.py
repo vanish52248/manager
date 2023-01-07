a93d88e1556ea4c2a7b31d1e252c8f42a4535850
vanish52248 <vanish52248@gmail.com>
2023-01-07 21:52:53 +0900

    一部修正

--- manager_api/manager_api/settings/production.py
+++ manager_api/manager_api/settings/production.py
@@ -28,7 +28,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'PORT': os.environ.get('DB_PORT')
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
